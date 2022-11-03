0c1a6e9fd39834598466c5bc762e830f45fe7fad
Julien West <jjwestt@outlook.com>
2022-11-03 15:47:42 -0700

    Added keys to Location and Product models

--- MVCWebApp/Models/LocationModel.cs
+++ MVCWebApp/Models/LocationModel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MVCWebApp.Models
 {
     public class LocationModel
     {
+        public long Id { get; set; }
+        private string _key;
+
+        public string Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = Regex.Replace(Country.ToLower() + Address.ToLower() + AreaCode, "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
         public string Country { get; set; }
         public string Address { get; set; }
         public int AreaCode { get; set; }
--- MVCWebApp/Models/ProductModel.cs
+++ MVCWebApp/Models/ProductModel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MVCWebApp.Models
 {
     public class ProductModel
     {
+        public long Id { get; set; }
+        private string _key;
+
+        public string Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = Regex.Replace(Name.ToLower() + Brand.ToLower() + Price, "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
         public string Name { get; set; }
         public string Brand { get; set; }
         public decimal Price { get; set; }
